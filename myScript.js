0aaa3eafe3a47efe17c40288dc6aa668a670d2bf
dosiat <119613118+dosiat@users.noreply.github.com>
2022-12-31 16:18:44 +0100

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,19 @@
+function chiuaua(){
+location.href = "https://www.google.com/search?q=chihuahua";
+}
+
+function labrador(){
+location.href = "https://www.google.com/search?q=labrador";
+}
+
+function mastiff(){
+location.href = "https://www.google.com/search?q=mastiff";
+}
+
+function apple(){
+location.href = "https://www.apple.com/app-store/";
+}
+
+function android(){
+location.href = "https://play.google.com/store/games";
+}
